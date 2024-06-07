bc8871b5ec4e1df1b54f5c267d95001118352c26
Jonas Mokawane <jonas.mokawane98@gmail.com>
2024-06-07 11:41:36 +0200

    Added useParams import and initialized params variable in VanDetail component

--- src/pages/VanDetail.jsx
+++ src/pages/VanDetail.jsx
@@ -1,5 +1,7 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
+    const params = useParams()
     return <h1>Van detail page goes here</h1>
 }
