3ffc3a7998872921f2c406605761c3625c3794aa
Jonas Mokawane <jonas.mokawane98@gmail.com>
2024-06-08 23:00:12 +0200

    "Added new routes and links for host dashboard, income, and reviews

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,10 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Vans from './pages/Vans/Vans';
 import VanDetail from './pages/Vans/VanDetail';
-import Layout from './components/Layout'
+import Layout from './components/Layout';
+import Dashboard from './pages/Host/Dashboard';
+import Income from './pages/Host/Income';
+import Reviews from './pages/Host/Reviews';
 import "./server"
 
 export default function App() {
@@ -18,6 +21,9 @@ export default function App() {
         <Route path="/About" element={<About />}/>
         <Route path="/Vans" element={<Vans />}/>
         <Route path="/Vans/:id" element={<VanDetail />}/>
+        <Route path="/host" element={<Dashboard />}/>
+        <Route path="/host/income" element={<Income />}/>
+        <Route path="/host/reviews" element={<Reviews />}/>
         </Route>
       </Routes>
     </BrowserRouter>
