b6c1983857f284ca517b64f0a11129ed8c7ce954
Jonas Mokawane <jonas.mokawane98@gmail.com>
2024-06-08 23:33:06 +0200

    Added HostLayout component and wrapped Dashboard route with it, and removed unnecessary JSX from Dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from "./pages/About";
 import Vans from "./pages/Vans/Vans";
 import VanDetail from "./pages/Vans/VanDetail";
 import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
 import Dashboard from "./pages/Host/Dashboard";
 import Income from "./pages/Host/Income";
 import Reviews from "./pages/Host/Reviews";
@@ -20,7 +21,8 @@ export default function App() {
           <Route path="/About" element={<About />} />
           <Route path="/Vans" element={<Vans />} />
           <Route path="/Vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />}>
+          <Route path="/host" element={<HostLayout />}>
+          <Route path="/host/" element={<Dashboard />} />
             <Route path="/host/income" element={<Income />} />
             <Route path="/host/reviews" element={<Reviews />} />
           </Route>
