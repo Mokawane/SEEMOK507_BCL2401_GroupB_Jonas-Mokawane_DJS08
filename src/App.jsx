87aae9f461bfd692eb59d277e6324e94161ed966
Jonas Mokawane <jonas.mokawane98@gmail.com>
2024-06-08 23:57:05 +0200

    Updated route definitions in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,15 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Vans" element={<Vans />} />
-          <Route path="/Vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<HostLayout />}>
-          <Route path="/host/" element={<Dashboard />} />
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="About" element={<About />} />
+          <Route path="Vans" element={<Vans />} />
+          <Route path="Vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout />}>
+          <Route index element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
