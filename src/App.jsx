c21560a0076071ba8e3c2377a7b2bbe69a3472ff
Jonas Mokawane <jonas.mokawane98@gmail.com>
2024-06-08 23:14:25 +0200

    Updated App.jsx and Dashboard.jsx files: reformatted code, added Outlet component to Dashboard, and nested routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Vans from './pages/Vans/Vans';
-import VanDetail from './pages/Vans/VanDetail';
-import Layout from './components/Layout';
-import Dashboard from './pages/Host/Dashboard';
-import Income from './pages/Host/Income';
-import Reviews from './pages/Host/Reviews';
-import "./server"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Vans from "./pages/Vans/Vans";
+import VanDetail from "./pages/Vans/VanDetail";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Host/Dashboard";
+import Income from "./pages/Host/Income";
+import Reviews from "./pages/Host/Reviews";
+import "./server";
 
 export default function App() {
-
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-        <Route path="/" element={<Home />}/>
-        <Route path="/About" element={<About />}/>
-        <Route path="/Vans" element={<Vans />}/>
-        <Route path="/Vans/:id" element={<VanDetail />}/>
-        <Route path="/host" element={<Dashboard />}/>
-        <Route path="/host/income" element={<Income />}/>
-        <Route path="/host/reviews" element={<Reviews />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Vans" element={<Vans />} />
+          <Route path="/Vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
-
