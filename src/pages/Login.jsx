95cbbf21ecee05e47bc49507736f47bb076b2e80
Jonas Mokawane <jonas.mokawane98@gmail.com>
2024-06-11 09:59:02 +0200

    Update AuthRequired and Login components to pass and use location state

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -11,6 +11,7 @@ export default function Login() {
   const [error, setError] = React.useState(null)
   const location = useLocation();
   const navigate = useNavigate()
+  const from = location.state?.from || "/host";
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -19,7 +20,7 @@ export default function Login() {
     .then(data => {
         setError(null)
         localStorage.setItem("loggedin", true)
-        navigate("/host", { replace: true})
+        navigate(from, "/host", { replace: true})
     })
     .catch(err => {
         setError(err)
